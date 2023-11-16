702fb8cddfd812a6e07b1eaf476e92a0285f221c
Ben <ben@armosec.io>
2023-11-16 18:12:08 +0200

    Enhancing system tests with basic test
    Signed-off-by: Ben <ben@armosec.io>

---
+++ system-tests/run.py
@@ -0,0 +1,88 @@
+import sys
+import requests
+import subprocess
+import time
+
+alert_manager_url = "http://localhost:9093/"
+
+def filter_alerts_by_label(alerts, label_key, label_value):
+    filtered_alerts = [alert for alert in alerts if label_key in alert['labels'] and alert['labels'][label_key] == label_value]
+    return filtered_alerts
+
+def get_active_alerts(alertmanager_url):
+    endpoint = f"{alertmanager_url}/api/v2/alerts?active=true"
+    try:
+        response = requests.get(endpoint)
+        response.raise_for_status()
+        alerts = response.json()
+        return alerts
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"OOps: Something Else: {err}")
+
+def basic_alert_test(namespace="kubecop-test"):
+    print("Running basic alert test")
+
+    # Create the namespace
+    subprocess.check_call(["kubectl", "create", "namespace", namespace])
+
+    try:
+        # Install nginx profile in kubernetes by applying the nginx profile yaml
+        subprocess.check_call(["kubectl", "-n", namespace , "apply", "-f", "dev/nginx/nginx-app-profile.yaml"])
+
+        # Install nginx in kubernetes by applying the nginx deployment yaml
+        subprocess.check_call(["kubectl", "-n", namespace , "apply", "-f", "dev/nginx/nginx-deployment.yaml"])
+
+        # Wait for nginx to be ready
+        subprocess.check_call(["kubectl", "-n", namespace , "wait", "--for=condition=ready", "pod", "-l", "app=nginx", "--timeout=120s"])
+
+        # Get the pod name of the nginx pod
+        nginx_pod_name = subprocess.check_output(["kubectl", "-n", namespace , "get", "pod", "-l", "app=nginx", "-o", "jsonpath='{.items[0].metadata.name}'"]).decode("utf-8").strip("'")
+
+        # Exec into the nginx pod and create a file in the /tmp directory
+        subprocess.check_call(["kubectl", "-n", namespace , "exec", nginx_pod_name, "--", "touch", "/tmp/nginx-test"])
+
+        # Wait for the alert to be fired
+        time.sleep(5)
+    except:
+        # Delete the namespace
+        subprocess.check_call(["kubectl", "delete", "namespace", namespace])
+        return 1
+
+    subprocess.check_call(["kubectl", "delete", "namespace", namespace])
+
+    # Get the active alerts
+    alerts = get_active_alerts(alert_manager_url)
+    if not alerts:
+        print("Could not get alerts")
+        return 1
+
+    alerts = filter_alerts_by_label(alerts, "alertname", "KubeCopRuleViolated")
+    alerts = filter_alerts_by_label(alerts, "rule_name", "Exec Whitelisted")
+
+    if len(alerts) == 0:
+        print("No alerts found")
+        return 1
+    else:
+        print("Found alerts %s" % alerts)
+        return 0
+
+def main():
+    global alert_manager_url
+    if len(sys.argv) > 1:
+        alert_manager_url = sys.argv[1]
+    result = basic_alert_test()
+    if result == 0:
+        print("Test passed")
+    else:
+        print("Test failed")
+    sys.exit(result)
+
+
+if __name__ == "__main__":
+    main()
