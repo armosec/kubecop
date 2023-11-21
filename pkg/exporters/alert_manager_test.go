9a218a5d7613d52f6b1f1144b4ebec58b1cbe6ee
Bezalel Brandwine <bez@armosec.io>
2023-11-21 18:56:13 +0200

    fix send alert test
    Signed-off-by: Bezalel Brandwine <bez@armosec.io>

--- pkg/exporters/alert_manager_test.go
+++ pkg/exporters/alert_manager_test.go
@@ -1,24 +1,34 @@
 package exporters
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/armosec/kubecop/pkg/engine/rule"
 	"github.com/kubescape/kapprofiler/pkg/tracing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSendAlert(t *testing.T) {
 	// Set up a mock Alertmanager server
+	recievedData := make(chan []byte, 1)
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		bodyData, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("Failed to read request body: %v", err)
+		}
+		recievedData <- bodyData
 	}))
 	defer server.Close()
 	// os.Setenv("ALERTMANAGER_URL", "localhost:9093")
 
 	// Create a new Alertmanager exporter
-	exporter := InitAlertManagerExporter(server.URL)
+	exporter := InitAlertManagerExporter(strings.Replace(server.URL, "http://", "", 1))
 	if exporter == nil {
 		t.Fatalf("Failed to create new Alertmanager exporter")
 	}
@@ -30,21 +40,26 @@ func TestSendAlert(t *testing.T) {
 		FailureEvent: &tracing.ExecveEvent{GeneralEvent: tracing.GeneralEvent{
 			ContainerName: "testcontainer", ContainerID: "testcontainerid", Namespace: "testnamespace", PodName: "testpodname"}},
 	})
+	bytesData := <-recievedData
 
-	// Assert that the alert was sent successfully
-	// expectedAlert := models.PostableAlert{
-	// 	StartsAt:    strfmt.DateTime(time.Now()),
-	// 	EndsAt:      strfmt.DateTime(time.Now().Add(time.Hour)),
-	// 	Annotations: map[string]string{"summary": "Description of the alert"},
-	// 	Alert: models.Alert{
-	// 		GeneratorURL: "http://github.com/armosec/kubecop",
-	// 		Labels:       map[string]string{"alertname": "MyAlertName", "severity": "critical"},
-	// 	},
-	// }
-	// expectedParams := alert.NewPostAlertsParams().WithContext(context.Background()).WithAlerts(models.PostableAlerts{&expectedAlert})
-	// assert.Equal(t, expectedParams, server.LastPostAlertsParams)
-	// assert.Nil(t, restapi.LastPostAlertsBody)
-	// assert.Equal(t, http.StatusOK, restapi.LastPostAlertsCode)
-	// assert.Equal(t, "application/json", restapi.LastPostAlertsContentType)
-	// assert.Equal(t, server.URL+"/api/v1/alerts", restapi.LastPostAlertsURL)
+	// Assert the request body is correct
+	alerts := []map[string]interface{}{}
+	if err := json.Unmarshal(bytesData, &alerts); err != nil {
+		t.Fatalf("Failed to unmarshal request body: %v", err)
+	}
+	assert.Equal(t, 1, len(alerts))
+	alert := alerts[0]
+	alertLabels := alert["labels"].(map[string]interface{})
+	assert.Equal(t, "KubeCopRuleViolated", alertLabels["alertname"])
+	assert.Equal(t, "testrule", alertLabels["rule_name"])
+	assert.Equal(t, "testcontainerid", alertLabels["container_id"])
+	assert.Equal(t, "testcontainer", alertLabels["container_name"])
+	assert.Equal(t, "testnamespace", alertLabels["namespace"])
+	assert.Equal(t, "testpodname", alertLabels["pod_name"])
+	assert.Equal(t, "", alertLabels["node_name"])
+	assert.Equal(t, "none", alertLabels["severity"])
+	assert.Equal(t, "Rule 'testrule' in 'testpodname' namespace 'testnamespace' failed", alert["annotations"].(map[string]interface{})["summary"])
+	assert.Equal(t, "Application profile is missing", alert["annotations"].(map[string]interface{})["message"])
+	assert.Equal(t, "http://github.com/armosec/kubecop", alert["generatorURL"])
+
 }
