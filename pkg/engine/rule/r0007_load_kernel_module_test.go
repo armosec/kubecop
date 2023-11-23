74618cd323ff76616e1437f50c2e4972c2575791
Amit Schendel <amitschendel@gmail.com>
2023-11-23 18:04:05 +0200

    Adding kernel module load rule
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

---
+++ pkg/engine/rule/r0007_load_kernel_module_test.go
@@ -0,0 +1,52 @@
+package rule
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kubescape/kapprofiler/pkg/tracing"
+)
+
+func TestR0007LoadKernelModule(t *testing.T) {
+	// Create a new rule
+	r := CreateRuleR0007LoadKernelModule()
+	// Assert r is not nil
+	if r == nil {
+		t.Errorf("Expected r to not be nil")
+	}
+
+	// Create a syscall event
+	e := &tracing.SyscallEvent{
+		GeneralEvent: tracing.GeneralEvent{
+			ContainerID: "test",
+			PodName:     "test",
+			Namespace:   "test",
+			Timestamp:   0,
+		},
+		Syscalls: []string{"test"},
+	}
+
+	ruleResult := r.ProcessEvent(tracing.SyscallEventType, e, nil)
+	if ruleResult != nil {
+		fmt.Printf("ruleResult: %v\n", ruleResult)
+		t.Errorf("Expected ruleResult to be nil since syscall is not init_module")
+	}
+
+	// Create a syscall event
+	e = &tracing.SyscallEvent{
+		GeneralEvent: tracing.GeneralEvent{
+			ContainerID: "test",
+			PodName:     "test",
+			Namespace:   "test",
+			Timestamp:   0,
+		},
+		Syscalls: []string{"init_module"},
+	}
+
+	ruleResult = r.ProcessEvent(tracing.SyscallEventType, e, nil)
+	if ruleResult == nil {
+		fmt.Printf("ruleResult: %v\n", ruleResult)
+		t.Errorf("Expected ruleResult to be Failure because of init_module is not allowed")
+	}
+
+}
