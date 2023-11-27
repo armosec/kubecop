a4a8e120ed86b94ca0cd39240641bef9e714af3d
Ben <ben@armosec.io>
2023-11-27 14:00:35 +0200

    Removing noisy prints
    Signed-off-by: Ben <ben@armosec.io>

--- pkg/engine/poll.go
+++ pkg/engine/poll.go
@@ -52,7 +52,8 @@ func (engine *Engine) Poll() {
 				for containerId, containerDetails := range getcontainerIdToDetailsCacheCopy() {
 					syscalls, err := engine.tracer.PeekSyscallInContainer(containerDetails.NsMntId)
 					if err != nil {
-						log.Printf("Failed to peek syscalls in container %s: %v\n", containerId, err)
+						// Disabled the log for now, it's too noisy (running containers and during shutdown this prints a lot)
+						// log.Printf("Failed to peek syscalls in container %s: %v\n", containerId, err)
 						continue
 					}
 					// Generate events for the syscalls and process them in the engine
