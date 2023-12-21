f7a0054d1b2aba08c84fad2ff18b53c495ede000
Amit Schendel <58078857+amitschendel@users.noreply.github.com>
2023-12-21 11:37:34 +0200

    Feature/finalize jitter (#108)
    * Bumping kapprofiler version
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Adding default jitter values and helm config
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Adding jitter in cm config
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Fixing system test
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Fixing jitter times and default values
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Adding fixed system tests
    
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    
    * Adding chart values
    
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    
    * Increasing the number of seconds to wait for an application profile
    
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    
    * Adding system tests fix
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    * Removing ipdb
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    
    ---------
    
    Signed-off-by: Amit Schendel <amits@armosec.io>
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- system-tests/creation_app_profile_memory_leak.py
+++ system-tests/creation_app_profile_memory_leak.py
@@ -23,8 +23,8 @@ def install_app_no_application_profile_no_leak(test_framework):
         # Get the pod name of the nginx pod
         nginx_pod_name = subprocess.check_output(["kubectl", "-n", namespace , "get", "pod", "-l", "app=nginx", "-o", "jsonpath='{.items[0].metadata.name}'"]).decode("utf-8").strip("'")
 
-        print("Waiting 130 seconds for the final application profile to be created")
-        time.sleep(130)
+        print("Waiting 150 seconds for the final application profile to be created")
+        time.sleep(150)
 
         get_proc = subprocess.run(["kubectl", "-n", namespace, "get", "applicationprofiles", f"pod-{nginx_pod_name}", "-oyaml"], capture_output=True)
         assert get_proc.returncode == 0 and 'kapprofiler.kubescape.io/final: "true"' in get_proc.stdout.decode("utf-8"), f"final applicationprofile ({get_proc.returncode}) did not got created {get_proc.stdout.decode('utf-8')}"
@@ -35,7 +35,7 @@ def install_app_no_application_profile_no_leak(test_framework):
         # Get kubecop pod name
         kc_pod_name = subprocess.check_output(["kubectl", "-n", "kubescape", "get", "pods", "-l", "app.kubernetes.io/name=kubecop", "-o", "jsonpath='{.items[0].metadata.name}'"], universal_newlines=True).strip("'")
         # Build query to get memory usage
-        query = 'sum(container_memory_working_set_bytes{pod="%s", container="kubecop"}) by (container)'%kc_pod_name
+        query = 'sum(container_memory_working_set_bytes{pod="%s"}) by (container)'%kc_pod_name
         timestamps, values = send_promql_query_to_prom("install_app_no_application_profile_no_leak_mem", query, time_start,time_end=time.time())
         save_plot_png("install_app_no_application_profile_no_leak_mem", values=values,timestamps=timestamps, metric_name='Memory Usage (bytes)')
 
